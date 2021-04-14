d085b4c15c5580a1b51ae3f055574245bb3db097
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-04-14 18:50:26 -0400

    feat: rmdx_docx
    * support for ... arguments passed to docx_document
    
    * New setups in skeleton
    
    * Improve base docx

--- R/rmdx_docx.R
+++ R/rmdx_docx.R
@@ -4,7 +4,9 @@
 #'
 #'  This format is based in \code{\link[officedown]{rdocx_document}}.
 #'
-#' @param toc logical See \code{\link[rmarkdown]{word_document}}
+#' @param toc [logical]: See \code{\link[rmarkdown]{word_document}}
+#' @param highlight [character]: See \code{\link[rmarkdown]{html_document}} highlight argument
+#' @param ... other arguments passed to \code{\link[officedown]{rdocx_document}}
 #'
 #' @return An R Markdown output format object to be passed to \code{\link[rmarkdown]{render}}
 #'
@@ -19,7 +21,7 @@
 #' ---
 #' ```
 #' }
-rmdx_docx = function(toc = TRUE) {
+rmdx_docx = function(toc = TRUE, highlight = 'pygments', ...) {
   # locations of resource files in the package
   pkg_resource = function(...) {
     system.file(..., package = "Rmdx")
@@ -30,7 +32,8 @@ rmdx_docx = function(toc = TRUE) {
   # call the base html_document function
   officedown::rdocx_document(
     toc = toc,
-    highlight = 'pygments',
-    reference_docx = base_docx
+    highlight = highlight,
+    reference_docx = base_docx,
+    ...
   )
 }
