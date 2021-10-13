47c81dd2161dcd480c5542107209aa0e175c7b19
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2021-10-13 14:51:50 -0400

    feat (rmdx_paper): new format added

---
+++ R/rmdx_paper.R
@@ -0,0 +1,25 @@
+#' Rmdx journal document (paper) format
+#'
+#' \lifecycle{experimental}
+#'
+#' This format is highly based in \code{\link[rticles]{arxiv_article}}.
+#'
+#' @param ... arguments to be passed to \code{\link[rmarkdown]{pdf_document}}
+#'
+#' @return An R Markdown output format object to be passed to \code{\link[rmarkdown]{render}}
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ---
+#' ...
+#' output: Rmdx::rmdx_paper
+#' ---
+#' }
+rmdx_paper = function(...) {
+  pkg_resource = function(...) {system.file(..., package = "Rmdx")}
+  template = pkg_resource("resources/pdf/template.tex")
+  rmarkdown::pdf_document(..., template = template)
+  fmt$inherits <- "pdf_document"
+  fmt
+}
